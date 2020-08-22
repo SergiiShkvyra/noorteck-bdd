3c884f7c287ce688e472ac0ad4b43a7e55fbd7b0
Owner <Owner@DESKTOP-C42QD54.fios-router.home>
2020-08-22 16:26:32 -0400

    New project pushed for trying out to set up jenkins jobs

---
+++ src/test/java/com/noorteck/qa/api/DemoC.java
@@ -0,0 +1,18 @@
+package com.noorteck.qa.api;
+
+public class DemoC {
+
+	public static void main(String[] args) {
+		String name = "Andrea";
+
+		System.out.println(name);
+		String studentName = null;
+
+ 
+		 studentName = "Andrea";
+		 
+		 
+		 System.out.println(studentName);
+	}
+
+}
---
+++ src/test/java/com/noorteck/qa/webservice/UsersAPI.java
@@ -0,0 +1,19 @@
+package com.noorteck.qa.webservice;
+
+import com.noorteck.qa.base.BaseClass;
+
+import io.restassured.path.json.JsonPath;
+
+public class UsersAPI extends BaseClass{
+	
+	public static void validateUserResponseBody() {
+		JsonPath jsonPath = response.jsonPath();
+
+		
+		apiSoftAssert.assertEquals(jsonPath.getString("name"), "Sergii");
+		apiSoftAssert.assertEquals(jsonPath.getString("job"), "SDET");
+		
+		apiSoftAssert.assertAll();
+	}
+	
+}
